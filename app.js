6c455a63cd2fddd8be97b13d90f1a5b132809b05
wm <wm798222@gmail.com>
2020-11-08 08:25:18 +0800

    commit for heroku

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const passwordDB = process.env.DB_PASSWORD;
+mongoose.connect("mongodb+srv://dbUser:" + passwordDB +"@cluster0.c6fst.mongodb.net/secretsDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.set('useCreateIndex', true);
 
